25cb33d165966c06d6665162b34a5f3a5d506949
Masatoshi Teruya <mah0x211@gmail.com>
2015-02-16 20:03:35 +0900

    add: test/tables_try.lua has been added.

---
+++ test/tables_try.lua
@@ -0,0 +1,29 @@
+local groonga = require('groonga');
+local path = './db/testdb';
+local g = groonga.new( path );
+local tbls = {};
+local nelts = 0;
+local name;
+
+-- cleanup
+if g then
+    g:remove();
+end
+g = ifNil( groonga.new( path, true ) );
+
+-- create table
+for i = 1, 10 do
+    name = 'test'.. i;
+    ifNil( g:tableCreate({ name = name }) );
+    tbls[name] = true;
+    nelts = nelts + 1;
+end
+
+-- getting all table
+for tbl, name in g:tables() do
+    tbls[name] = nil;
+    nelts = nelts - 1;
+end
+ifNotEqual( nelts, 0 );
+
+g:remove();
