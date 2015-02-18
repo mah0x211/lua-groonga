43aa2d18dd2c01a34e4c4c890a7310528fef3da4
Masatoshi Teruya <mah0x211@gmail.com>
2015-02-18 11:22:58 +0900

    add: test/column_rename_try.lua has been added.

---
+++ test/column_rename_try.lua
@@ -0,0 +1,34 @@
+local groonga = require('groonga');
+local path = './db/testdb';
+local g = groonga.new( path );
+local name = 'testColumn';
+local t,c;
+
+if g then
+    g:remove();
+end
+
+g = ifNil( groonga.new( path, true ) );
+t = ifNil( g:tableCreate({ name = 'test' }) );
+c = ifNil( t:columnCreate({
+    name = name,
+    path = "./db/col",
+    -- 'SCALAR' | 'VECTOR' | 'INDEX'
+    type ='SCALAR',
+    valType = 'UINT32',
+    -- 'PERSISTENT'
+    persistent = true,
+    -- 'ZLIB' | 'LZ4'
+    compress = 'ZLIB',
+    -- 'WITH_WEIGHT'
+    withWeight = false,
+    -- 'WITH_SECTION'
+    withSection = false,
+    -- 'WITH_POSITION'
+    withPosition = false
+}) );
+-- rename
+ifNotNil( c:rename( name .. 'change' ) );
+ifNotEqual( c:name(), name .. 'change' );
+
+g:remove();
