0fc90a3c2756941978357648a93b0294570f9184
Masatoshi Teruya <mah0x211@gmail.com>
2015-02-20 15:16:04 +0900

    change: new_try.lua has been updated.

--- test/new_try.lua
+++ test/new_try.lua
@@ -1,24 +1,26 @@
 local groonga = require('groonga');
 local path = './db/testdb';
 local g = groonga.new( path );
+local ref;
 
 -- remove testdb
 if g then
     g:remove();
 end
 
--- not exists
-ifNotNil( groonga.new( path ) );
--- create db
-ifNil( groonga.new( path, true ) );
--- open db
-ifNil( groonga.new( path ) );
 -- create temporary db
 ifNil( groonga.new( nil, true ) );
 
+-- not exists
+ifNotNil( groonga.new( path ) );
+-- create db
+g = ifNil( groonga.new( path, true ) );
+-- open db
+ref = ifNil( groonga.new( path ) );
+ifNotEqual( g, ref );
+
 -- remove db
-g = groonga.new( path );
-if g then
-    g:remove();
-end
+g:remove();
+ifTrue( ref:remove() );
+ifNotNil( groonga.new( path ) );
 
